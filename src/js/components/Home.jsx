588a59b280993b59f28e1796e8e9075937f6c422
Alba-cdlg <alba.cdelagrana@icloud.com>
2025-03-19 21:32:38 +0000

    creada lista y elimina

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,20 +1,47 @@
-import React from "react";
+import { useState } from "react";
+// import React { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+  const [inputValue, setImputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="container">
       <h1>Todos</h1>
       <ul>
-		<input type="text" placeholder="What needs to be done?"/>
-        <li>Wash my hands</li>
-        <li>Make the bed</li>
-        <li>Eat</li>
-        <li>Walk the dog</li>
+        <li>
+          <input
+            type="text"
+            onChange={(e) => setImputValue(e.target.value)}
+            value={inputValue}
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                setTodos(todos.concat([inputValue]));
+                setImputValue("");
+              }
+            }}
+            placeholder="What needs to be done?"
+          ></input>
+        </li>
+        {todos.map((item, index) => (
+          <li>
+            {item} {""}
+            <i
+              class="bi bi-x"
+              onClick={() =>
+                setTodos(
+                  todos.filter((t, currentIndex) => index != currentIndex)
+                )
+              }
+            ></i>
+          </li>
+        ))}
       </ul>
+      <div>1 Item left</div>
     </div>
   );
 };
